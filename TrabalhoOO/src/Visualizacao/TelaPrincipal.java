f7381317c4b80bc1ec67146e9016b66980295d49
FilhoDoan <doangalvaofilho@gmail.com>
2023-01-11 10:28:40 -0300

    Interface Grafica

--- TrabalhoOO/src/Visualizacao/TelaPrincipal.java
+++ TrabalhoOO/src/Visualizacao/TelaPrincipal.java
@@ -1,5 +1,4 @@
 package Visualizacao;
-
 import javax.swing.*;
 
 
@@ -13,18 +12,17 @@ public class TelaPrincipal {
 		telaPrincipal.setSize(1024, 600);
 		telaPrincipal.setLocation(171, 83);
 		telaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		
+		//Tela de Menu 
 		JMenuBar menu = new JMenuBar();
 		telaPrincipal.setJMenuBar(menu);
 		
+		//Itens da Tela de Menu 
 		JMenu menuCompra = new JMenu("Comprar");
 		JMenu menuVenda = new JMenu("Vender");
-		
 		menu.add(menuCompra);
 		menu.add(menuVenda);
 		
-		JLabel label = new JLabel();
-		label.setSize(300,300);
 		
 		telaPrincipal.setVisible(true);
 
